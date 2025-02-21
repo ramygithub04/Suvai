f6f05fd2b9719baebef1f8418ee1d73fd8c84274
ramygithub04 <ramyagomega04@gmail.com>
2025-02-21 20:46:57 +0530

    Basic page components created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
+import Navbar from './components/navbar'
+
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Login />
+      <Register />
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
