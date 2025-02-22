ced0da52d3141a1e4adfad9066434d744f6d3234
ramygithub04 <ramyagomega04@gmail.com>
2025-02-22 13:50:43 +0530

    Registeration page done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import './Register.css';
 
-const Register = () => {
+const Login = () => {
+  const navigate = useNavigate(); // Initialize navigate function
+
   return (
     <div>
-      Registeration page
+      <form onSubmit={(e) => {
+        e.preventDefault(); // Prevent page refresh
+        navigate("/home"); // Navigate to home page
+      }}>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Re-enter Password" />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">I'm not a robot</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Login;
