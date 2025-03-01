a9b4984a6d24e26d20e0874921d4ecd67b145ab8
ramygithub04 <ramyagomega04@gmail.com>
2025-03-01 13:37:14 +0530

    changes for backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
+import axios from 'axios';
 
-const Login = () => {
-  const navigate = useNavigate();
+const Login = (setIsLoggedIn) => {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const navigate = useNavigate();
+  
+    const handleLogin = async ()=>{
+      try{
+        await axios.post("http://localhost:3000/login",{email,password})
+        localStorage.setItem("username",res.data.username)
+        setIsLoggedIn(true)
+        navigate("/home");
+      } catch(error){
+        alert("Invalid Credentials");
+      }
+    };
 
   return (
     <div>
@@ -11,6 +25,7 @@ const Login = () => {
         e.preventDefault();
         navigate("/home");
       }}>
+      
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
@@ -24,7 +39,7 @@ const Login = () => {
           <label className="form-check-label" htmlFor="exampleCheck1">I'm not a robot</label>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+        </form>
     </div>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Register.css';
+import axios from "axios";
 
-const Register = () => {
+const Register = ({setIsLoggedIn}) => {
+  const [username,setUsername]=useState("");
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
   const navigate = useNavigate();
+
+  const handleRegister = async ()=>{
+    await axios.post("http://localhost:3000/register",{username,email,password})
+    localStorage.setItem("username",username)
+    setIsLoggedIn(true)
+    navigate("/home");
+  };
 
   return (
     <div>
@@ -11,24 +22,25 @@ const Register = () => {
         e.preventDefault();
         navigate("/home");
       }}>
+      
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <label htmlFor="exampleInputEmail1">Username</label>
+          <input type="String" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Email</label>
+          <input type="email" className="form-control" id="exampleInputPassword1" placeholder="Email" />
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
           <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Re-enter Password" />
         </div>
         <div className="form-group form-check">
           <input type="checkbox" className="form-check-input" id="exampleCheck1" />
           <label className="form-check-label" htmlFor="exampleCheck1">I'm not a robot</label>
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+        </form>
     </div>
   );
 }
